4c50d67069c4fa1bb4bd060ee9caff0a34b82c9c
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-06-02 15:03:57 +0400

    CI/CD Test

--- assets/js/pages/sai-kitchen/constant.js
+++
@@ -1,6 +0,0 @@
-export let baseURL = "https://backendsaikitchen.azurewebsites.net/Api";
-export let userId;
-export let userToken;
-export let branchId;
-export let branchRoleId;
-export let inquiryWorkscopeId;
