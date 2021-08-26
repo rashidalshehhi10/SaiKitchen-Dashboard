b89db7d04d8b78d38e00b0ba723d8e6fc3119124
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-08-26 12:57:33 +0400

    updated base js

--- demo1/dist/assets/js/pages/sai-kitchen/base.js
+++ demo1/dist/assets/js/pages/sai-kitchen/base.js
@@ -681,21 +681,26 @@ document.getElementById("kt_header").innerHTML =
                   </div>
                   <!--end::Container-->
                `;
+			   try {
+				   
 document.getElementById("kt_footer").innerHTML = `<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
-                  <!--begin::Copyright-->
-                  <div class="text-dark order-2 order-md-1">
-                    <span class="text-muted font-weight-bold mr-2">2021©</span>
-                    <a href="http://www.sai-group.ae/" target="_blank" class="text-dark-75 text-hover-primary">Sai Kitchen</a>
-                  </div>
-                  <!--end::Copyright-->
-                  <!--begin::Nav-->
-                  <div class="nav nav-dark">
-                    <a href="http://www.sai-group.ae/about/" target="_blank" class="nav-link pl-0 pr-5">About</a>
-                    <a href="http://www.sai-group.ae/" target="_blank" class="nav-link pl-0 pr-5">Team</a>
-                    <a href="http://www.sai-group.ae/contact-us/" target="_blank" class="nav-link pl-0 pr-0">Contact</a>
-                  </div>
-                  <!--end::Nav-->
-                  </div>`;
+<!--begin::Copyright-->
+<div class="text-dark order-2 order-md-1">
+  <span class="text-muted font-weight-bold mr-2">2021©</span>
+  <a href="http://www.sai-group.ae/" target="_blank" class="text-dark-75 text-hover-primary">Sai Kitchen</a>
+</div>
+<!--end::Copyright-->
+<!--begin::Nav-->
+<div class="nav nav-dark">
+  <a href="http://www.sai-group.ae/about/" target="_blank" class="nav-link pl-0 pr-5">About</a>
+  <a href="http://www.sai-group.ae/" target="_blank" class="nav-link pl-0 pr-5">Team</a>
+  <a href="http://www.sai-group.ae/contact-us/" target="_blank" class="nav-link pl-0 pr-0">Contact</a>
+</div>
+<!--end::Nav-->
+</div>`;
+			   } catch (error) {
+				   
+			   }
 document.getElementById("kt_quick_user").innerHTML =
                   ` <!--begin::Header-->
                   <div class="offcanvas-header d-flex align-items-center justify-content-between pb-5">
