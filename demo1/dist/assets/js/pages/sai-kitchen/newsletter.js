51075ebb9afaf76db04b9c43de98d81688d832b2
mhdmhd <mhdmhd@proits-it.com>
2021-11-17 16:49:38 +0200

    design & newsletter

--- demo1/dist/assets/js/pages/sai-kitchen/newsletter.js
+++ demo1/dist/assets/js/pages/sai-kitchen/newsletter.js
@@ -244,6 +244,15 @@ var KTAppsUsersListDatatable = function() {
 			</span>\
 		</button>\
 		';
+        action += '\
+        <button type="button"  onclick="sendEmail(\'' + data.newsletterId + '\');"  class="btn btn-sm btn-default btn-text-primary btn-hover-primary btn-icon" title="Send Email">\
+        <span class="svg-icon svg-icon-md">\
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">\
+            <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>\
+            </svg>\
+        </span>\
+    </button>\
+    ';
                             //                     if (workscopePermission >= 5) {
                             //                         action += '\
                             // 	<button type="button"  onclick="deleteCustomerById(\'' + baseURL + '\',\'' + data.customerId + '\');"  class="btn btn-sm btn-default btn-text-primary btn-hover-primary btn-icon" title="Delete">\
@@ -692,3 +701,89 @@ $.ajax({
 
 
 });
+
+
+var _buttonSpinnerClasses = 'spinner spinner-right spinner-white pr-15';
+$('#kt_button_sendmail').click( function() {
+        var form = KTUtil.getById('kt_sendmail');
+        var formSubmitUrl = KTUtil.attr(form, 'action');
+        var formSubmitButton = KTUtil.getById('kt_button_sendmail');
+
+        if (!form) {
+            return;
+        }
+
+        FormValidation
+            .formValidation(
+                form, {
+                    fields: {
+                        email: {
+                            validators: {
+                                notEmpty: {
+                                    message: 'Email Address is required'
+                                }
+                            }
+                        }, 
+                    },
+                    plugins: {
+                        trigger: new FormValidation.plugins.Trigger(),
+                        submitButton: new FormValidation.plugins.SubmitButton(),
+                        bootstrap: new FormValidation.plugins.Bootstrap({
+                        })
+                    }
+                }
+            )
+            .on('core.form.valid', function() {
+                // Show loading state on button
+                KTUtil.btnWait(formSubmitButton, _buttonSpinnerClasses, "Please wait");
+                var email = {
+                    "newsletterId":parseInt( document.getElementById('nlId').innerHTML),
+                    "emailto": document.getElementById('email').value,
+                  };
+            
+                const data = JSON.stringify(email);
+
+                console.log(data);
+                $.ajax({
+                    type: "Post",
+                    url: baseURL + '/Newsletter/SendEmail',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'userId': user.data.userId,
+                        'Access-Control-Allow-Origin': '*',
+                    },
+                    data: data,
+                    success: function(response) {
+                        console.log(response);
+                        $('#exampleModalCenter').modal('hide');
+                        Swal.fire({
+							text: 'Mail Sent',
+							icon: "success",
+							buttonsStyling: false,
+							confirmButtonText: "Ok, got it!",
+							customClass: {
+								confirmButton: "btn font-weight-bold btn-light-primary"
+                            }
+							}).then(function () {
+                                KTUtil.scrollTop();
+                                window.location.replace("newsletter.html");
+                            });
+                    },
+                    error: function(XMLHttpRequest, textStatus, errorThrown) {
+                        $('#exampleModalCenter').modal('hide');
+                        Swal.fire({
+							text: "Error in sending",
+							icon: "error",
+							buttonsStyling: false,
+							confirmButtonText: "Ok, got it!",
+							customClass: {
+								confirmButton: "btn font-weight-bold btn-light-primary"
+							}
+						}).then(function () {
+							KTUtil.scrollTop();
+                            window.location.replace("newsletter.html");
+						});
+                    }
+                });
+            });
+    });
