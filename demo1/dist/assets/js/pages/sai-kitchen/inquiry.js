23311937866dd19b8ede11b40ab636892c0877d3
MHDYSF <eng.daibus@gmail.com>
2021-12-18 09:01:35 +0200

    Inquiry

--- demo1/dist/assets/js/pages/sai-kitchen/inquiry.js
+++ demo1/dist/assets/js/pages/sai-kitchen/inquiry.js
@@ -1401,15 +1401,15 @@ jQuery(document).ready(function() {
                         
                             document.getElementById('inquirybyUser').innerHTML +=`
     
-                                    <div class="col-xl-2">
+                                    <div class="col-xl-3">
                                                     <!--begin::Tiles Widget 4-->
-                                                <div class="card card-custom gutter-b" style="height: 150px;">
+                                                <div class="card card-custom gutter-b" style="height: 110px;background-color: #755043;"">
                                                     <!--begin::Body-->
                                                     <div id="divclkUser"  style="border-radius: .42rem;cursor: pointer;" class="card-body d-flex flex-column" onclick=searchInquiryManaged(`+response.data[counter].userId+`) >
                                                         <!--begin::Stats-->
                                                         <div class="flex-grow-1">
-                                                            <div class="font-weight-bolder font-size-h3" id="">`+response.data[counter].inquiriesCount+`</div>
-                                                            <div class="text-dark-50 font-weight-bold">Inquires Account By:  `+response.data[counter].user+`</div>                                                                               
+                                                            <div class="text-white display-3 font-weight-bold" id="">`+response.data[counter].inquiriesCount+`</div>
+                                                            <div style="opacity: 90%;font-size: 0.9rem;" class="text-white font-weight-bold">Inquires Account By:  `+response.data[counter].user+`</div>                                                                               
                                                         </div>
                                                         <!--end::Stats-->
                                                     </div>
