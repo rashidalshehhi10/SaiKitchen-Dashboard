18696d476857917f59aca7836b4f94b9c729a490
MHDYSF <eng.daibus@gmail.com>
2021-12-28 12:34:17 +0200

    customer graph

--- demo1/dist/assets/js/pages/sai-kitchen/base.js
+++ demo1/dist/assets/js/pages/sai-kitchen/base.js
@@ -1766,6 +1766,18 @@ jQuery(document).ready(function () {
    <span id="customerCountId" class="badge badge-circle badge-white" style="align-self: center;"></span>
 </a>
 </li>
+<li class="menu-item menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
+<a href="gochart.html" class="menu-link menu-toggle">
+	<span class="svg-icon menu-icon">
+	
+   <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+   <img src="/assets//media/svg/saikitchen/customers.svg"/>
+   <!--end::Svg Icon-->
+	</span>
+	<span class="menu-text">Customer Graph</span>
+   <span id="customerCountId" class="badge badge-circle badge-white" style="align-self: center;"></span>
+</a>
+</li>
 `;
       }
       if (permission.includes(6)) {
