8f31d1a99a5931c8bbd7900fec0d95fc3a1980ef
MHDYSF <eng.daibus@gmail.com>
2021-12-29 12:11:14 +0200

    flowchart

--- demo1/dist/assets/js/pages/sai-kitchen/gochart.js
+++ demo1/dist/assets/js/pages/sai-kitchen/gochart.js
@@ -1,25 +1,6 @@
 
 jQuery(document).ready(function() {
 
-  function init(){
-
-      var $ = go.GraphObject.make;
-   
-   var myDiagram =
-     $(go.Diagram, "myDiagramDiv",
-       { // enable Ctrl-Z to undo and Ctrl-Y to redo
-         "undoManager.isEnabled": true
-       });
-   
-   myDiagram.model = new go.Model(
-     [ // for each object in this Array, the Diagram creates a Node to represent it
-       { key: "Alpha" },
-       { key: "Beta" },
-       { key: "Gamma" }
-     ]);
-  }
-
-  init();
 
 
 
